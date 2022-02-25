bbad7f05a7d30e4012a8017e66ab21207958ea39
YYJ <naki5087@naver.com>
2022-02-25 12:21:50 +0900

    입력한 숫자가 검사에 통과하면 배열에 집어넣기
    -중복검사도 같이 수행

--- src/Lotto.java
+++ src/Lotto.java
@@ -5,6 +5,8 @@ public class Lotto {
 	public static void main(String[] args) {
 //		6개의 숫자 입력받기
 		
+		int[] myNumbers = new int[6];
+		
 		Scanner myScanner = new Scanner(System.in);
 		for(int i =0; i < 6; i++) {
 			while(true) {
@@ -12,7 +14,16 @@ public class Lotto {
 			int inputNum = myScanner.nextInt();
 			
 			boolean isRangeOK = (1 <= inputNum) &&(inputNum <= 45);
-			if(isRangeOK) {
+			boolean isDupok = true;
+			for(int myNum : myNumbers) {
+				if(myNum == inputNum) {
+					isDupok = false;
+					break;
+				}
+			}
+			if(isRangeOK && isDupok) {
+				myNumbers[i] = inputNum;
+				
 				break;
 			}
 			}
