eab6b9925e4b6fd75f8e3dc15a8e7bf3f01aa2bd
YYJ <naki5087@naver.com>
2022-02-25 12:12:05 +0900

    하나의 스캐너를 입력 받아서 번호 생성

--- src/Lotto.java
+++ src/Lotto.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 
 public class Lotto {
 
+	public static void main(String[] args) {
+//		6개의 숫자 입력받기
+		
+		Scanner myScanner = new Scanner(System.in);
+		for(int i =0; i < 6; i++) {
+			System.out.print((i+1) +"번째 숫자 : ");
+			int inputNum = myScanner.nextInt();
+		}
+	}
 }
